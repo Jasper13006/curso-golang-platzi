dc1dcae9beb52188d6dd948c648aef3d663b05d2
Jasper13006 <jasper13006@gmail.com>
2022-02-21 09:03:32 -0500

    variables y constantes

--- src/main.go
+++ src/main.go
@@ -3,5 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	// Declaracion de constantes
+	const pi float64 = 3.14159265358979323846
+	const pi2 = 3.4
+	fmt.Println("El valor de pi es: ", pi)
+	fmt.Println("El valor de pi es: ", pi2)
+
+	// Declaracion de variables enteras
+	var altura int = 10
+	base := 12 // los dos puntos dicen que la variable nunca fue asignada antes
+	var area int
+
+	fmt.Println("El valor de la base es: ", base)
+	fmt.Println("El valor de la altura es: ", altura)
+	fmt.Println("El valor de la area es: ", area)
+
+	// Zero values
+	var a int
+	var b float64
+	var c string
+	var d bool
+	fmt.Println("El valor de a es: ", a)
+	fmt.Println("El valor de b es: ", b)
+	fmt.Println("El valor de c es: ", c)
+	fmt.Println("El valor de d es: ", d)
+
+	// Area cuadrado.
+	const basdeCuadrado = 10
+	areaCuadrado := basdeCuadrado * basdeCuadrado
+	fmt.Println("El valor de la area del cuadrado es: ", areaCuadrado)
 }
