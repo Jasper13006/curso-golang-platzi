113042643fe83a23e0965c3017e06e6042aea891
Jasper13006 <jasper13006@gmail.com>
2022-02-26 17:48:21 -0500

    Datos primitivos

--- src/main.go
+++ src/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
 	// Declaracion de constantes
 	// const pi float64 = 3.14159265358979323846
@@ -33,28 +31,55 @@ func main() {
 	// areaCuadrado := basdeCuadrado * basdeCuadrado
 	// fmt.Println("El valor de la area del cuadrado es: ", areaCuadrado)
 
-	x := 10
-	y := 50
-	// suma
-	fmt.Println("El valor de x + y es: ", x+y)
+	// x := 10
+	// y := 50
+	// // suma
+	// fmt.Println("El valor de x + y es: ", x+y)
 
-	// resta
-	fmt.Println("El valor de x - y es: ", x-y)
+	// // resta
+	// fmt.Println("El valor de x - y es: ", x-y)
 
-	// multiplicacion
-	fmt.Println("El valor de x * y es: ", x*y)
+	// // multiplicacion
+	// fmt.Println("El valor de x * y es: ", x*y)
 
-	// division
-	fmt.Println("El valor de x / y es: ", x/y)
+	// // division
+	// fmt.Println("El valor de x / y es: ", x/y)
 
-	// modulo
-	fmt.Println("El valor de x % y es: ", x%y)
+	// // modulo
+	// fmt.Println("El valor de x % y es: ", x%y)
 
-	// incremento
-	x++
-	fmt.Println("El valor de x es: ", x)
+	// // incremento
+	// x++
+	// fmt.Println("El valor de x es: ", x)
 
-	// decremento
-	x--
-	fmt.Println("El valor de x es: ", x)
+	// // decremento
+	// x--
+	// fmt.Println("El valor de x es: ", x)
+
+	//Numeros enteros
+	//int = Depende del OS (32 o 64 bits)
+	//int8 = 8bits = -128 a 127
+	//int16 = 16bits = -2^15 a 2^15-1
+	//int32 = 32bits = -2^31 a 2^31-1
+	//int64 = 64bits = -2^63 a 2^63-1
+
+	//Optimizar memoria cuando sabemos que el dato simpre va ser positivo
+	//uint = Depende del OS (32 o 64 bits)
+	//uint8 = 8bits = 0 a 127
+	//uint16 = 16bits = 0 a 2^15-1
+	//uint32 = 32bits = 0 a 2^31-1
+	//uint64 = 64bits = 0 a 2^63-1
+
+	//numeros decimales
+	// float32 = 32 bits = +/- 1.18e^-38 +/- -3.4e^38
+	// float64 = 64 bits = +/- 2.23e^-308 +/- -1.8e^308
+
+	//textos y booleanos
+	//string = ""
+	//bool = true or false
+
+	//numeros complejos
+	//Complex64 = Real e Imaginario float32
+	//Complex128 = Real e Imaginario float64
+	//Ejemplo : c:=10 + 8i
 }
