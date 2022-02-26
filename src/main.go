cc4ea2daf8595794eb22eff301fdb43dda7885e4
Jasper13006 <jasper13006@gmail.com>
2022-02-26 17:44:06 -0500

    operadores

--- src/main.go
+++ src/main.go
@@ -4,32 +4,57 @@ import "fmt"
 
 func main() {
 	// Declaracion de constantes
-	const pi float64 = 3.14159265358979323846
-	const pi2 = 3.4
-	fmt.Println("El valor de pi es: ", pi)
-	fmt.Println("El valor de pi es: ", pi2)
+	// const pi float64 = 3.14159265358979323846
+	// const pi2 = 3.4
+	// fmt.Println("El valor de pi es: ", pi)
+	// fmt.Println("El valor de pi es: ", pi2)
 
 	// Declaracion de variables enteras
-	var altura int = 10
-	base := 12 // los dos puntos dicen que la variable nunca fue asignada antes
-	var area int
+	// var altura int = 10
+	// base := 12 // los dos puntos dicen que la variable nunca fue asignada antes
+	// var area int
 
-	fmt.Println("El valor de la base es: ", base)
-	fmt.Println("El valor de la altura es: ", altura)
-	fmt.Println("El valor de la area es: ", area)
+	// fmt.Println("El valor de la base es: ", base)
+	// fmt.Println("El valor de la altura es: ", altura)
+	// fmt.Println("El valor de la area es: ", area)
 
 	// Zero values
-	var a int
-	var b float64
-	var c string
-	var d bool
-	fmt.Println("El valor de a es: ", a)
-	fmt.Println("El valor de b es: ", b)
-	fmt.Println("El valor de c es: ", c)
-	fmt.Println("El valor de d es: ", d)
+	// var a int
+	// var b float64
+	// var c string
+	// var d bool
+	// fmt.Println("El valor de a es: ", a)
+	// fmt.Println("El valor de b es: ", b)
+	// fmt.Println("El valor de c es: ", c)
+	// fmt.Println("El valor de d es: ", d)
 
 	// Area cuadrado.
-	const basdeCuadrado = 10
-	areaCuadrado := basdeCuadrado * basdeCuadrado
-	fmt.Println("El valor de la area del cuadrado es: ", areaCuadrado)
+	// const basdeCuadrado = 10
+	// areaCuadrado := basdeCuadrado * basdeCuadrado
+	// fmt.Println("El valor de la area del cuadrado es: ", areaCuadrado)
+
+	x := 10
+	y := 50
+	// suma
+	fmt.Println("El valor de x + y es: ", x+y)
+
+	// resta
+	fmt.Println("El valor de x - y es: ", x-y)
+
+	// multiplicacion
+	fmt.Println("El valor de x * y es: ", x*y)
+
+	// division
+	fmt.Println("El valor de x / y es: ", x/y)
+
+	// modulo
+	fmt.Println("El valor de x % y es: ", x%y)
+
+	// incremento
+	x++
+	fmt.Println("El valor de x es: ", x)
+
+	// decremento
+	x--
+	fmt.Println("El valor de x es: ", x)
 }
