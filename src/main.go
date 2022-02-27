433ab2a4c065125e674fcbccad7d400acfe9b6d2
Jasper13006 <jasper13006@gmail.com>
2022-02-26 22:52:35 -0500

    funcioneas

--- src/main.go
+++ src/main.go
@@ -2,101 +2,27 @@ package main
 
 import "fmt"
 
-func main() {
-	// Declaracion de constantes
-	// const pi float64 = 3.14159265358979323846
-	// const pi2 = 3.4
-	// fmt.Println("El valor de pi es: ", pi)
-	// fmt.Println("El valor de pi es: ", pi2)
-
-	// Declaracion de variables enteras
-	// var altura int = 10
-	// base := 12 // los dos puntos dicen que la variable nunca fue asignada antes
-	// var area int
-
-	// fmt.Println("El valor de la base es: ", base)
-	// fmt.Println("El valor de la altura es: ", altura)
-	// fmt.Println("El valor de la area es: ", area)
-
-	// Zero values
-	// var a int
-	// var b float64
-	// var c string
-	// var d bool
-	// fmt.Println("El valor de a es: ", a)
-	// fmt.Println("El valor de b es: ", b)
-	// fmt.Println("El valor de c es: ", c)
-	// fmt.Println("El valor de d es: ", d)
-
-	// Area cuadrado.
-	// const basdeCuadrado = 10
-	// areaCuadrado := basdeCuadrado * basdeCuadrado
-	// fmt.Println("El valor de la area del cuadrado es: ", areaCuadrado)
-
-	// x := 10
-	// y := 50
-	// // suma
-	// fmt.Println("El valor de x + y es: ", x+y)
-
-	// // resta
-	// fmt.Println("El valor de x - y es: ", x-y)
-
-	// // multiplicacion
-	// fmt.Println("El valor de x * y es: ", x*y)
-
-	// // division
-	// fmt.Println("El valor de x / y es: ", x/y)
-
-	// // modulo
-	// fmt.Println("El valor de x % y es: ", x%y)
-
-	// // incremento
-	// x++
-	// fmt.Println("El valor de x es: ", x)
-
-	// // decremento
-	// x--
-	// fmt.Println("El valor de x es: ", x)
-
-	//Numeros enteros
-	//int = Depende del OS (32 o 64 bits)
-	//int8 = 8bits = -128 a 127
-	//int16 = 16bits = -2^15 a 2^15-1
-	//int32 = 32bits = -2^31 a 2^31-1
-	//int64 = 64bits = -2^63 a 2^63-1
-
-	//Optimizar memoria cuando sabemos que el dato simpre va ser positivo
-	//uint = Depende del OS (32 o 64 bits)
-	//uint8 = 8bits = 0 a 127
-	//uint16 = 16bits = 0 a 2^15-1
-	//uint32 = 32bits = 0 a 2^31-1
-	//uint64 = 64bits = 0 a 2^63-1
-
-	//numeros decimales
-	// float32 = 32 bits = +/- 1.18e^-38 +/- -3.4e^38
-	// float64 = 64 bits = +/- 2.23e^-308 +/- -1.8e^308
-
-	//textos y booleanos
-	//string = ""
-	//bool = true or false
-
-	//numeros complejos
-	//Complex64 = Real e Imaginario float32
-	//Complex128 = Real e Imaginario float64
-	//Ejemplo : c:=10 + 8i
-
-	helloMesage := "Hello"
-	worldMessage := "World"
-
-	fmt.Println(helloMesage + " " + worldMessage)
-	fmt.Println(helloMesage + " " + worldMessage)
-
-	fmt.Printf("%s %s\n", helloMesage, worldMessage)
-	nombre := "Platzi"
-	cursons := 500
-
-	fmt.Printf("%v tiene %d cursos\n", nombre, cursons)
-
-	message := fmt.Sprintf("%v tiene %d cursos", nombre, cursons)
+func normalFuncion(message string) {
 	fmt.Println(message)
+}
+
+func tripeArgument(a, b int, c string) {
+	fmt.Println(a, b, c)
+}
+
+func returnValue(a int) int {
+	return a * 2
+}
+
+func doubleReturn(a int) (c, d int) {
+	return a, a * 2
+}
+
+func main() {
+	normalFuncion("Hello World")
+	tripeArgument(1, 2, "Hello")
+	fmt.Println(returnValue(3))
+
+	value1, value2 := doubleReturn(3)
+	fmt.Println(value1, value2)
 }
