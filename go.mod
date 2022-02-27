1a92666fae78a930777d16c846089714ad9d6c8e
Jasper13006 <jasper13006@gmail.com>
2022-02-27 14:48:02 -0500

    limpiar librerias que no se usan

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module curso-golang-platzi
 
 go 1.13
-
-require (
-	github.com/labstack/echo v3.3.10+incompatible
-	github.com/labstack/echo/v4 v4.6.3 // indirect
-)
--- src/main.go
+++ src/main.go
@@ -1,18 +1,15 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/labstack/echo"
-)
+import "fmt"
 
 func main() {
 
-	e := echo.New()
+	// e := echo.New()
 
-	// Routes
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(":1323"))
+	// // Routes
+	// e.GET("/", func(c echo.Context) error {
+	// 	return c.String(http.StatusOK, "Hello, World!")
+	// })
+	// e.Logger.Fatal(e.Start(":1323"))
+	fmt.Println("Hola")
 }
